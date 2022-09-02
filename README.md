b6095307eacfef817b282406ba09cdf6ffe09dab
negativefix <alper.buluc@gmail.com>
2022-09-02 15:58:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Deceleration distance calculator for Space Engine (Game)
+A command-line utility to compute the distance needed to decelerate using Torricelli's equation.
+
+# Compilation
+`gcc decel_dist.c -lm -o decel_dist`
+
+# Usage
+Usage: `decel_dist [a] <current_velocity> <max_speed>` - both the current velocity and max speed are in kilometers
+
+
+```bash
+decel_dist 2843 0.1
+# Output: Distance needed to decelerate is 40384820.000000 kilometers
+
+decel_dist -a 2843 0.1
+# Output: Distance needed to decelerate is 0.269956 AU
+```
+
---
+++ decel_dist.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+void print_usage() {
+	puts("Usage: decel_dist <current_velocity> <max_speed> - both units are kilometers");
+}
+
+void print_program_desc() {
+	puts("Space Engine: Deceleration distance calculator");
+}
+
+double kmtoau(double distance_km) {
+	return distance_km / 149597870;
+}
+
+int main(int argc, char *argv[]) {
+	
+	if (argc < 3) {
+		print_usage();
+		return 1;
+	}
+
+	int c;
+	bool to_au = false;
+	while((c = getopt(argc, argv, "a")) != -1) {
+		switch (c) {
+			case 'a':
+				to_au = true;
+				break;
+
+		}
+	}
+
+	// check for the missing arguments to prevent seg fault
+	if (argc == 3 && optind == 2) {
+		print_program_desc();
+		print_usage();	
+		return 1;
+	}
+
+	double current_velocity = atof(argv[optind]);
+	double max_speed = atof(argv[optind + 1]);
+
+	if (current_velocity == 0.0 || max_speed == 0.0) {
+		print_program_desc();
+		print_usage();
+		return 1;
+	}
+	
+	double distance_to_decel = (0 - pow(current_velocity, 2)) / (2 * max_speed);
+	distance_to_decel = to_au == true ? kmtoau(distance_to_decel) : distance_to_decel;
+	char *unit = to_au == true ? "AU" : "kilometers"; 
+	print_program_desc();	
+	printf("Distance needed to decelerate is %f %s\n", fabs(distance_to_decel), unit);
+}
